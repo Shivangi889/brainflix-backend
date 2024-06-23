ded53949e766408d49d3c1c116203a0a9c9f832d
shivangi <cseshivangi599@gmail.com>
2024-06-23 20:59:25 +0530

    cors error resolved

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const app = express();
 const videosdetails = require("./routes/videos")
 const port = process.env.PORT;
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+const allowedOrigins = ['https://app.netlify.com', 'https://chic-tulumba-33a046.netlify.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+
 app.use(express.json());
   
 app.use("/",videosdetails)
